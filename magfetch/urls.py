dab9e638193468853e3c4808c8b9c42c5facac0b
Numan Ibn Mazid <numanibnmazid@gmail.com>
2019-02-12 08:00:28 +0600

    First initialization in GIT

--- magfetch/urls.py
+++ magfetch/urls.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import (
+handler400, handler403, handler404, handler500
+)
 from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
 ]
+
+handler400 = 'megfetch.views.bad_request'
+handler403 = 'megfetch.views.permission_denied'
+handler404 = 'megfetch.views.page_not_found'
+handler500 = 'megfetch.views.server_error'
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
