eb1ee307b9fc9b5dd3771ab8eca4c3059dae96ff
Numan Ibn Mazid <numanibnmazid@gmail.com>
2019-02-12 08:16:37 +0600

    Modified

--- magfetch/urls.py
+++ magfetch/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import HomeView
+from .views import HomeView, error_404
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
 ]
+
+handler404 = error_404
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- magfetch/views.py
+++ magfetch/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
+
+def error_404(request):
+    data = {}
+    return render(request,'404.html', data)
