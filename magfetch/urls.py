5f142783b2af0dd3fc2178e76c0e4c55c1778a9c
Numan Ibn Mazid <numanibnmazid@gmail.com>
2019-02-12 08:08:05 +0600

    Modified

--- magfetch/urls.py
+++ magfetch/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import HomeView
+from .views import HomeView, handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
 ]
+
+# handler404 = megfetch.views.handler404
+handler404 = handler404
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
