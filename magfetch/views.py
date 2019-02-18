500c2df7fac4680d299a3d10b5c3fc1344ab66ba
Numan Ibn Mazid <numanibnmazid@gmail.com>
2019-02-18 10:44:56 +0600

    Setting Changed

--- magfetch/views.py
+++ magfetch/views.py
@@ -12,14 +12,5 @@ from accounts.models import UserProfile
 @method_decorator(login_required, name='dispatch')
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        user = request.user
-        user_profile_filter = UserProfile.objects.filter(user=user)
-        if user_profile_filter.exists():
-            profile = user_profile_filter.first()
-            if profile.role == 4:
-                return render(request, "landing/pages/home.html")
-            if profile.role == 0 or profile.role == 1 or profile.role == 2 or profile.role == 3:
-                return render(request, "pages/home.html")
-            if profile.user.is_staff == True:
-                return render(request, "pages/home.html")
-        return render(request, "anonymous.html")
+        
+        return render(request, "pages/home.html")
