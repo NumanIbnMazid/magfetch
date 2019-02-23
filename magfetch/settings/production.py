912fe1498d7d5392dced71422ee99db88e050ecb
Numan Ibn Mazid <numanibnmazid@gmail.com>
2019-02-24 05:32:05 +0600

    Re Migrations

--- magfetch/settings/production.py
+++ magfetch/settings/production.py
@@ -1,8 +1,8 @@
 from magfetch.settings.common import *
 import django_heroku
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'qrd526_f3-yqr6)lf+zp6onky2oj$@6el!ua4fa2f3a(+ij$$@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'qrd526_f3-yqr6)lf+zp6onky2oj$@6el!ua4fa2f3a(+ij$$@'
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['magfetch.herokuapp.com', '.magfetch.com']
@@ -10,10 +10,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-DEBUG_COLLECTSTATIC = os.environ.get('DEBUG_COLLECTSTATIC')  # SET to 1
-DISABLE_COLLECTSTATIC = os.environ.get('DISABLE_COLLECTSTATIC')  # SET to 1
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# DEBUG_COLLECTSTATIC = os.environ.get('DEBUG_COLLECTSTATIC')  # SET to 1
+# DISABLE_COLLECTSTATIC = os.environ.get('DISABLE_COLLECTSTATIC')  # SET to 1
 
 ADMINS = (
     ('admin', 'admin@magfetch.com'),
