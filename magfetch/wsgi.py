490d3db2841a18b25a97bc1f40fad738544eef66
Numan Ibn Mazid <nelmannuman@gmail.com>
2019-03-11 03:46:00 +0600

    Setting Module Changed

--- magfetch/wsgi.py
+++ magfetch/wsgi.py
@@ -5,5 +5,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.development')
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.production')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.pythonanywhere')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.development')
     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.production')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.pythonanywhere')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
