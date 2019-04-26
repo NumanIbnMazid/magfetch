10913476ea425f411a5ca669f69dafe4c53a4613
NumanIbnMazid <numanibnmazid@gmail.com>
2019-04-26 07:46:16 +0600

    Polished

--- manage.py
+++ manage.py
@@ -3,9 +3,9 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.development')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.development')
     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.production')
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.pythonanywhere')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magfetch.settings.pythonanywhere')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
